1f0bef093139cf9e68d6d747a36472121ef0891d
gregoryNatali <gregory.natali4@gmail.com>
2023-10-21 15:06:24 -0300

    feat: contactStruct definition

---
+++ src/contactStruct.h
@@ -0,0 +1,11 @@
+#ifndef CONTACT_STRUCT_DEFINITION_HEADER
+#define CONTACT_STRUCT_DEFINITION_HEADER
+
+struct  contact {
+    int id;
+    int phoneNumber;
+    int contactType;
+    char* name;
+  };
+
+#endif
